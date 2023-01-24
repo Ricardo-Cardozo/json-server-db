d7ca355dd6af675beca5e61de52db3f39f480c25
Ricardo Cardozo <ricardo_alexandrecardozo@hotmail.com>
2023-01-24 20:45:04 -0300

    json-server succesfully

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const jServer = require('json-server')
 const server = jServer.create()
 const router = jServer.router('db.json')
 const middlewares = jServer.defaults()
-const port = 3000
+const port = process.env.PORT ||3000
 
 console.log("deu certo");
 
