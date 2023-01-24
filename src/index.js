a941cee30957537d4474a915561a703a46716bba
Ricardo Cardozo <ricardo_alexandrecardozo@hotmail.com>
2023-01-24 20:35:47 -0300

    versiopn 1

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ const router = jServer.router('db.json')
 const middlewares = jServer.defaults()
 const port = 3000
 
+console.log("deu certo");
+
 server.use(middlewares)
 server.use(router)
-server.listen(port)
+server.listen(port, () => {
+  console.log("deu certo");
+})
